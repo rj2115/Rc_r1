767907212ae20cff67f126bfc372b37bedf74757
Rohan Jadhav <87532969+rj2115@users.noreply.github.com>
2021-11-07 23:44:35 +0530

    Update views.py

--- register/views.py
+++ register/views.py
@@ -1,26 +1,31 @@
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-#from .forms import *
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,  login
-from .models import *
+from .models import Profile
 # Create your views here.
 def register(request):
- 
-  if request.method=="POST":
-    if request.POST.get('firstN') and request.POST.get('lastN') and request.POST.get('username') and request.POST.get('email') and request.POST.get('password') and request.POST.get('password1'):
-     post=Profile()
-     post.firstN=request.POST.get('firstN')
-     post.lastN=request.POST.get('lastN')
-     post.username=request.POST.get('username')
-     post.email=request.POST.get('email')
-     post.password=request.POST.get('password')
-     post.password1=request.POST.get('password1')
-     post.save()
-     return render(request,'register/register.html')
-  else:
+    if(request.method == 'POST'):
+        firstname = request.POST['firstN']
+        lastname = request.POST['lastN']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        user = User.objects.create_user(username, '',password)
+        user.firstN = firstname
+        user.lastN = lastname
+        user.save()
+        password = hash(password)
+        password1 = hash(password1)
+      
+        profile = Profile(firstN=firstname,lastN=lastname,username=username,email=email,password=password,password1=password1)
+        profile.save()
+        return render(request, 'register/que_page.html',{'username':username})
     return render(request,'register/register.html')
 
     
@@ -29,42 +34,32 @@ def register(request):
    
  
 
-def que(request):
-  return render(request,'register/que_page.html')
 
 
 def Logins(request):
-  if not request.user.is_authenticated:
+  if request.method=="post":
+    username=request.POST['username']
+    password=request.POST['password']
+    user=authenticate(username=username,password=password)
+    if user is not None:
+      login(request,user)
+      return render(request,'register/que_page.html',{'username':username})
+    else:
+      return render(request,'register/login.html')
+  return render(request,'register/login.html')
+@login_required(login_url=None)
+
+
+def que(request):
   
+  return render(request,'register/que_page.html')
 
-   if request.method=="post":
-    if request.POST.get('username') and request.POST.get('password'):
-
-
-     fm=Login()
-     fm.username=request.POST.get('username')
-     fm.password=request.POST.get('password')
-    
-     uname=fm.cleaned_data['username']
-     upass=fm.cleaned_data['password']
-     user = authenticate(username=uname,password=upass)
-     if user is not None:
-      login(request, user)
-      messages.success(request,'LOGGED IN SUCCESFULLY!!!')
-      return HttpResponseRedirect('/que_page/')
-     else:
-       messages.success(request,"The field is required")
-      
-     return render (request,'register/login.html')
-   
-      
-  
       
        
-  else:
-     fm=Login()
-  return render(request,'register/login.html')
+  
 
   
+
+
 
 
